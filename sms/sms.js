b8aa55ab7d6e06ae309b8f97d0385e238f2a0036
Rens van Woudenberg <rensvanw@outlook.com>
2017-04-03 16:42:27 +0200

    removed old microservioces, added texting support, currently building authentication with sms support

--- sms/sms.js
+++ sms/sms.js
@@ -1,12 +1,14 @@
 const config = require('./config');
 const client = require('twilio')(config.accountSid, config.authToken);
+const randomID = require('random-id');
 
 module.exports = function sms(options){
 
     this.add({role:'sms',cmd:'send'}, sendTextMessage);
+    this.add({role:'sms',cmd:'createCode'}, createSMSCode)
 
     function sendTextMessage(msg,respond){
-        let message = msg.message;
+        let message = "test";
         let to = msg.to;
         let from = config.sendingNumber;
         client.messages.create({
@@ -21,6 +23,34 @@ module.exports = function sms(options){
                 respond({message:"SMS has been send!"});
             }
         });
+    }
+
+    function createSMSCode(msg,respond){
+        let code = randomID(6,'0');
+        let email = msg.email;
+        this.act('role:user,cmd:addSMSCodeToUser',{
+      email:email,
+      code: code
+    },function(err,user){
+      if(err){
+        respond(err,null);
+      }
+      else if(!user){
+        respond(null,{
+          succes: false,
+          message: "User could not be found!"
+        });
+      }
+      else if(user){
+         if(err){
+                respond(err,null);
+            }
+            else{
+                respond;
+            }
+
+      }
+    });
     }
 
 };
