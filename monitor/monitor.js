70947b9aceeffcde3787bfc6ef599de9fd31afdc
Rens van Woudenberg <rens.van.woudenberg@qnh.nl>
2017-04-06 10:46:37 +0200

    Changed montitor microservice

--- monitor/monitor.js
+++ monitor/monitor.js
@@ -1,7 +1,7 @@
 var HOST = process.env.HOST || process.argv[2] || '127.0.0.1'
 var BASES = (process.env.BASES || process.argv[3] || '127.0.0.1:39000,127.0.0.1:39001').split(',')
 
-require('seneca')()//({log: 'silent'})
+require('seneca')({log: 'silent'})
   .use('mesh',{
     bases: BASES,
     host: HOST,
