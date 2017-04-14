345369f1572aff46bf629c778119e806c0798ec2
Rens van Woudenberg <rensvanw@outlook.com>
2017-04-14 12:59:21 +0200

    changed hosts docker

--- docker/user/user-service-mongo.js
+++ docker/user/user-service-mongo.js
@@ -7,11 +7,11 @@ seneca
   .use('entity')
   .use('basic')
   .use(require('./user'))
-  .use('mongo-store', {
+  /*.use('mongo-store', {
   uri: 'mongodb://rensvanw:zb74jt3bzn.@ds157439.mlab.com:57439/qnh'
   //uri: 'mongodb://127.0.0.1:27017/mongodb'
   
-})
+})*/
 .use('zipkin-tracer', {sampling:1})
   .use('mesh',{
     pin: 'role:user,cmd:*',
