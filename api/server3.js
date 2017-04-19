438cebaf8296df287c555db46afa8226275ae85b
Rens van Woudenberg <rensvanw@outlook.com>
2017-04-19 10:47:41 +0200

    api

--- api/server3.js
+++ api/server3.js
@@ -19,18 +19,6 @@ server.connection({
     host: host
 })
 
-seneca.add({role: 'test', cmd: 'test'}, function(args, done) {
-  console.log('Recieved a request');
-  done(null, {worked: true});
-});
-
-seneca.act('role:web', {use: {
-  prefix: '/api/1.0',
-  pin: {role:'test', cmd: '*'},
-  map: {
-    test: true
-  }
-}});
 
 seneca
   .use('mesh', {
