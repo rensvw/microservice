5495036de337779bb4859c92af20cdd04e05737a
Rens van Woudenberg <rensvanw@outlook.com>
2017-04-19 13:24:39 +0200

    Changed api port numbe mesh network

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-var PORT = process.env.PORT || process.argv[2] || 3000;
+var PORT = process.env.PORT || process.argv[2] || 0;
 var HOST = process.env.HOST || process.argv[2] || '127.0.0.1';
 var BASES = (process.env.BASES || process.argv[3] || '127.0.0.1:39000,127.0.0.1:39001').split(',');
 var SILENT = process.env.SILENT || process.argv[4] || 'true';
