0dc51fb00580f05845bb26e12bc04d8df838af30
Rens van Woudenberg <rensvanw@outlook.com>
2017-04-20 12:57:54 +0200

    added production version angular

--- docker/api/server.js
+++ docker/api/server.js
@@ -4,7 +4,7 @@ var BASES = (process.env.BASES || process.argv[3] || '127.0.0.1:39000,127.0.0.1:
 var SILENT = process.env.SILENT || process.argv[4] || 'true';
 
 const Chairo = require('chairo');
-const Seneca = require('seneca')();
+const Seneca = require('seneca');
 const tag = 'api';
 const Joi = require('joi');
 const Hapi = require('hapi');
@@ -12,21 +12,106 @@ const Handlebars = require('handlebars');
 const Boom = require('boom');
 const Bcrypt = require('bcrypt');
 const CookieAuth = require('hapi-auth-cookie');
+const Rif = require('rif');
+const Inert = require('inert');
+const HapiSwagger = require('hapi-swagger');
+var Good = require('good');
+var Vision = require('vision');
 
+const options = {
+    info: {
+            'title': 'SecurityPoC API Documentation',
+            'version': '1',
+        }
+    };
 
 // create new server instance
 const server = new Hapi.Server();
+var rif = Rif()
 
+
+var host = rif(HOST) || HOST
 
 // add server’s connection information
 server.connection({
-  port: 3000
+  port: PORT,
+  host: host
 });
 
 // register plugins to server instance
-const plugins = require('./plugins');
 
-server.register(plugins, function (err) {
+server.register([
+  {
+    register: Inert
+  },
+    {
+      register: Vision
+    },
+    {
+        register: HapiSwagger,
+        options: options
+    },
+ 
+  {
+    register: Good,
+    options: {
+      ops: {
+        interval: 10000
+      },
+      reporters: {
+        console: [
+          {
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [ { log: '*', response: '*', request: '*' } ]
+          },
+          {
+            module: 'good-console'
+          },
+          'stdout'
+        ]
+      }
+    }
+  },
+  {
+    register: CookieAuth
+  },
+  { register: Chairo,
+    options: {
+        seneca: Seneca({
+            tag: tag,
+            internal: {
+                logger: require('seneca-demo-logger')
+            },
+            debug: {
+                short_logs: true
+            }
+        })
+        .use('zipkin-tracer', {sampling:1})
+    }
+},{
+  register: require('wo'),
+  options:{
+    bases: BASES,
+    route: [
+        {path: '/api/', method: 'get'},      
+        {path: '/documentation', method: 'get'},      
+        {path: '/api/login', method: 'post'},
+        {path: '/api/login-email', method: 'post'},
+        {path: '/api/login-sms', method: 'post'},
+        {path: '/api/logout', method: 'get'},
+        {path: '/api/signup', method: 'post'},
+        {path: '/api/verify-email', method: 'post'},
+        {path: '/api/verify-sms', method: 'post'},
+        
+    ],
+    sneeze: {
+      host: host,
+      silent: JSON.parse(SILENT),
+      swim: {interval: 1111}
+    }
+  }
+}], function (err) {
   if (err) {
     server.log('error', 'failed to install plugins');
     throw err;
@@ -46,6 +131,167 @@ server.register(plugins, function (err) {
   });
 
   server.log('info', 'Registered auth strategy: cookie auth')
+
+
+// Tests if user is logged in!
+const testAuth = (request, reply) => {
+  if (request.auth.isAuthenticated) {
+    return reply({
+      auth: 'yessss'
+    });
+  }
+  return reply({
+    auth: 'nooooo'
+  });
+}
+
+// Function for logging in!
+const login = (request, reply) => {
+  if (request.auth.isAuthenticated) {
+    return reply({
+      message: "You're already authenticated!"
+    });
+  }
+  let email = request.payload.email;
+  let password = request.payload.password;
+  server.seneca.act('role:auth,cmd:authenticate', {
+    password: password,
+    email: email
+  }, function (err, respond) {
+    if (err) {
+      return reply(Boom.badRequest(respond(err)));
+    } else if (respond.succes) {
+      request.cookieAuth.set(respond.user);
+      return reply(respond);
+    } else if (!respond.succes) {
+      return reply(Boom.unauthorized('Username or password is wrong!'));
+    }
+  });
+};
+
+const authorizeAndSendSMSCode = (request,reply) => {
+  if (request.auth.isAuthenticated) {
+    return reply({
+      message: "You're already authenticated!"
+    });
+  }
+  let email = request.payload.email;
+  let password = request.payload.password;
+  server.seneca.act('role:auth,cmd:authenticate,tfa:sms', {
+    password: password,
+    email: email
+  }, function (err, respond) {
+    if (err) {
+      return reply(Boom.badRequest(respond(err)));
+    } else if (respond.succes == true) {
+      
+      return reply(respond);
+    } else if (respond.succes == false) {
+      return reply(Boom.unauthorized('Username or password is wrong!'));
+    }
+  });
+}
+
+const verifySMSCodeAndLogin = (request, reply) => {
+  if (request.auth.isAuthenticated) {
+    return reply({
+      message: "You're already authenticated!"
+    });
+  }
+  let code = request.payload.code;
+  let uuid = request.payload.uuid;
+  server.seneca.act('role:auth,cmd:verify,tfa:sms', {
+    code: code,
+    uuid: uuid
+  }, function (err, respond) {
+    if (err) {
+      reply(err);
+    } else if (respond.succes == true) {
+      request.cookieAuth.set(respond.user);
+      reply({succes:respond.succes,
+      message: respond.message})
+    } else if (respond.succes == false) {
+     reply({succes:respond.succes,
+      message: respond.message})
+    }
+  });
+};
+
+//Function for logging out!
+const logout = (request, reply) => {
+  request.cookieAuth.clear();
+  return reply('You are logged out!');
+}
+
+// Function for registering!
+const signUp = (request, reply) => {
+  let email = request.payload.email;
+  let fullName = request.payload.fullName;
+  let password = request.payload.password;
+  let countryCode = request.payload.countryCode;
+  let mobilePhoneNumber = request.payload.mobilePhoneNumber;
+  server.seneca.act('role:auth,cmd:signup', {
+    email: email,
+    fullName: fullName,
+    password: password,
+    countryCode: countryCode,
+    mobilePhoneNumber: mobilePhoneNumber
+  }, function (err, respond) {
+    if (err) {
+      return reply(Boom.badRequest(respond(err, null)));
+    } else {
+      return reply(respond);
+    }
+  });
+};
+
+const authorizeAndSendEmailCode = (request,reply) => {
+  if (request.auth.isAuthenticated) {
+    return reply({
+      message: "You're already authenticated!"
+    });
+  }
+  let email = request.payload.email;
+  let password = request.payload.password;
+  server.seneca.act('role:auth,cmd:authenticate,tfa:email', {
+    password: password,
+    email: email
+  }, function (err, respond) {
+    if (err) {
+      return reply(Boom.badRequest(respond(err)));
+    } else if (respond.succes == true) {
+      
+      return reply(respond);
+    } else if (respond.succes == false) {
+      return reply(Boom.unauthorized('Username or password is wrong!'));
+    }
+  });
+}
+
+const verifyEmailCodeAndLogin = (request, reply) => {
+  if (request.auth.isAuthenticated) {
+    return reply({
+      message: "You're already authenticated!"
+    });
+  }
+  let code = request.payload.code;
+  let uuid = request.payload.uuid;
+  server.seneca.act('role:auth,cmd:verify,tfa:email', {
+    code: code,
+    uuid: uuid
+  }, function (err, respond) {
+    if (err) {
+      reply(err);
+    } else if (respond.succes == true) {
+      request.cookieAuth.set(respond.user);
+      reply({succes:respond.succes,
+      message: respond.message})
+    } else if (respond.succes == false) {
+     reply({succes:respond.succes,
+      message: respond.message})
+    }
+  });
+};
 
   // Routes
   server.route([{
@@ -220,6 +466,20 @@ server.register(plugins, function (err) {
 
   server.log('info', 'Routes registered');
 
+
+// Set up mesh network
+server.seneca
+  .use('mesh', {
+    host: host,
+    bases: BASES,
+    sneeze: {
+      silent: JSON.parse(SILENT),
+      swim: {
+        interval: 1111
+      }
+    }
+  });
+
   // start your server after plugin registration
   server.start(function (err) {
     if (err) {
@@ -234,176 +494,3 @@ server.register(plugins, function (err) {
 
 
 
-// Tests if user is logged in!
-const testAuth = (request, reply) => {
-  if (request.auth.isAuthenticated) {
-    return reply({
-      auth: 'yessss'
-    });
-  }
-  return reply({
-    auth: 'nooooo'
-  });
-}
-
-// Function for logging in!
-const login = (request, reply) => {
-  if (request.auth.isAuthenticated) {
-    return reply({
-      message: "You're already authenticated!"
-    });
-  }
-  let email = request.payload.email;
-  let password = request.payload.password;
-  server.seneca.act('role:auth,cmd:authenticate', {
-    password: password,
-    email: email
-  }, function (err, respond) {
-    if (err) {
-      return reply(Boom.badRequest(respond(err)));
-    } else if (respond.succes) {
-      //request.cookieAuth.set(respond.user);
-      return reply(respond);
-    } else if (!respond.succes) {
-      return reply(Boom.unauthorized('Username or password is wrong!'));
-    }
-  });
-};
-
-const authorizeAndSendSMSCode = (request,reply) => {
-  if (request.auth.isAuthenticated) {
-    return reply({
-      message: "You're already authenticated!"
-    });
-  }
-  let email = request.payload.email;
-  let password = request.payload.password;
-  server.seneca.act('role:auth,cmd:authenticate,tfa:sms', {
-    password: password,
-    email: email
-  }, function (err, respond) {
-    if (err) {
-      return reply(Boom.badRequest(respond(err)));
-    } else if (respond.succes == true) {
-      
-      return reply(respond);
-    } else if (respond.succes == false) {
-      return reply(Boom.unauthorized('Username or password is wrong!'));
-    }
-  });
-}
-
-const verifySMSCodeAndLogin = (request, reply) => {
-  if (request.auth.isAuthenticated) {
-    return reply({
-      message: "You're already authenticated!"
-    });
-  }
-  let code = request.payload.code;
-  let uuid = request.payload.uuid;
-  server.seneca.act('role:auth,cmd:verify,tfa:sms', {
-    code: code,
-    uuid: uuid
-  }, function (err, respond) {
-    if (err) {
-      reply(err);
-    } else if (respond.succes == true) {
-      request.cookieAuth.set(respond.user);
-      reply({succes:respond.succes,
-      message: respond.message})
-    } else if (respond.succes == false) {
-     reply({succes:respond.succes,
-      message: respond.message})
-    }
-  });
-};
-
-//Function for logging out!
-const logout = (request, reply) => {
-  request.cookieAuth.clear();
-  return reply('You are logged out!');
-}
-
-// Function for registering!
-const signUp = (request, reply) => {
-  let email = request.payload.email;
-  let fullName = request.payload.fullName;
-  let password = request.payload.password;
-  let countryCode = request.payload.countryCode;
-  let mobilePhoneNumber = request.payload.mobilePhoneNumber;
-  server.seneca.act('role:auth,cmd:signup', {
-    email: email,
-    fullName: fullName,
-    password: password,
-    countryCode: countryCode,
-    mobilePhoneNumber: mobilePhoneNumber
-  }, function (err, respond) {
-    if (err) {
-      return reply(Boom.badRequest(respond(err, null)));
-    } else {
-      return reply(respond);
-    }
-  });
-};
-
-const authorizeAndSendEmailCode = (request,reply) => {
-  if (request.auth.isAuthenticated) {
-    return reply({
-      message: "You're already authenticated!"
-    });
-  }
-  let email = request.payload.email;
-  let password = request.payload.password;
-  server.seneca.act('role:auth,cmd:authenticate,tfa:email', {
-    password: password,
-    email: email
-  }, function (err, respond) {
-    if (err) {
-      return reply(Boom.badRequest(respond(err)));
-    } else if (respond.succes == true) {
-      
-      return reply(respond);
-    } else if (respond.succes == false) {
-      return reply(Boom.unauthorized('Username or password is wrong!'));
-    }
-  });
-}
-
-const verifyEmailCodeAndLogin = (request, reply) => {
-  if (request.auth.isAuthenticated) {
-    return reply({
-      message: "You're already authenticated!"
-    });
-  }
-  let code = request.payload.code;
-  let uuid = request.payload.uuid;
-  server.seneca.act('role:auth,cmd:verify,tfa:email', {
-    code: code,
-    uuid: uuid
-  }, function (err, respond) {
-    if (err) {
-      reply(err);
-    } else if (respond.succes == true) {
-      request.cookieAuth.set(respond.user);
-      reply({succes:respond.succes,
-      message: respond.message})
-    } else if (respond.succes == false) {
-     reply({succes:respond.succes,
-      message: respond.message})
-    }
-  });
-};
-
-
-// Set up mesh network
-seneca
-  .use('mesh', {
-    host: HOST,
-    bases: BASES,
-    sneeze: {
-      silent: JSON.parse(SILENT),
-      swim: {
-        interval: 1111
-      }
-    }
-  });
---
+++ docker/front/front.js
@@ -0,0 +1,111 @@
+
+"use strict"
+var HOST = process.env.HOST || process.argv[2] || '127.0.0.1'
+var BASES = (process.env.BASES || process.argv[3] || '127.0.0.1:39000,127.0.0.1:39001').split(',');
+var SILENT = process.env.SILENT || process.argv[4] || 'true'
+
+var Hapi = require('hapi')
+var Rif = require('rif')
+
+
+var server = new Hapi.Server()
+var rif = Rif()
+
+
+var host = rif(HOST) || HOST
+
+
+server.connection({ 
+  port: 8000
+})
+
+server.register(require('inert'))
+
+server.register({
+  register: require('wo'),
+  options: {
+    bases: BASES,
+    sneeze: {
+	host: host,
+	silent: JSON.parse(SILENT),
+        swim: {interval: 1111}
+      }
+  }
+})
+
+server.route({
+  method: 'GET', path: '/api/', 
+  handler: {
+    wo: {
+      passThrough: true
+    }
+  }
+})
+
+server.route({
+  method: 'POST', path: '/api/login', 
+  handler: {
+    wo: {
+      passThrough: true
+    }
+  }
+})
+
+server.route({
+  method: 'POST', path: '/api/login-email', 
+  handler: {
+    wo: {
+      passThrough: true
+    }
+  }
+})
+
+server.route({
+  method: 'POST', path: '/api/login-sms', 
+  handler: {
+    wo: {
+      passThrough: true
+    }
+  }
+})
+
+server.route({
+  method: 'GET', path: '/api/logout', 
+  handler: {
+    wo: {
+      passThrough: true
+    }
+  }
+})
+
+server.route({
+  method: 'POST', path: '/api/signup', 
+  handler: {
+    wo: {
+      passThrough: true
+    }
+  }
+})
+
+server.route({
+  method: 'POST', path: '/api/verify-email', 
+  handler: {
+    wo: {
+      passThrough: true
+    }
+  }
+})
+
+server.route({
+  method: 'POST', path: '/api/verify-sms', 
+  handler: {
+    wo: {
+      passThrough: true
+    }
+  }
+})
+
+
+server.start(function(){
+  console.log('front',server.info.uri)
+})
